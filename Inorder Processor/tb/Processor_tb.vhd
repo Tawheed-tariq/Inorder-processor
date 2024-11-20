182caddf8b3961c8f5a8a3df6f08cc2465462847
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-11-20 19:04:10 +0530

    comparator

--- Inorder Processor/tb/Processor_tb.vhd
+++ Inorder Processor/tb/Processor_tb.vhd
@@ -1,4 +1,3 @@
--- Processor_tb.vhd
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
--- Inorder Processor/tb/Top_tb.vhd
+++ Inorder Processor/tb/Top_tb.vhd
@@ -1,4 +1,3 @@
--- Top_tb.vhd
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
